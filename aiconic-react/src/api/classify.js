311fcdba2c1d8e8a2cc13d0704a1964c52262f83
Dan Mo <danmo1337@gmail.com>
2018-04-22 10:29:24 -0400

    updates api host url

--- aiconic-react/src/api/classify.js
+++ aiconic-react/src/api/classify.js
@@ -1,7 +1,5 @@
 export async function uploadImageAsync(file) {
-    let apiUrl = 'http://localhost:5000/classify';
-    console.log("FILE");
-    console.log(file[0]);
+    let apiUrl = 'http://danielmo.com:5000/classify';
     let formData = new FormData();
     formData.append('classify', file[0]);
 
